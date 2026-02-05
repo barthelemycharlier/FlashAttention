935cd2be32ec404099ae2ed93e84bcda52c902a9
barthelemy.charlier <barthelemy.charlier@gmail.com>
2026-02-05 12:32:46 +0000

    comit

--- triton-tutorials/add.py
+++ triton-tutorials/add.py
@@ -16,8 +16,46 @@ def add(x,y):
     # that will run in parallel
     # code runs hundreds of time in parallel accross the SM on the gpu
     # number of element in our tensor
-    n_elements = output.numel() # gives the number of elements in a tensor
-    grid = lambda meta_parameters: (triton.cdiv(n_elements,meta["BLOCK_SIZE"]))  # needs to be a tuple of entries and these needs to be integers
+    n_elements = output.numel() # gives the number of elements in a tensor # could use x.numel() as well
+    grid = lambda meta_parameters: (triton.cdiv(n_elements,meta_parameters["BLOCK_SIZE"]),)  # needs to be a tuple of entries and these needs to be integers
+
+    # here that tuple is (4,), cdvi(m,n) = (m+(n-1))//n
+    # if n_element is not cleanly divislible then cdiv handles it ? 
+
+    #here we need to index that function, we need to subset it ? using our launching grid
+    add_kernel[grid](
+        x,
+        y,
+        output,
+        n_elements,
+        BLOCK_SIZE=1024
+    )
+    return output
+
+
+@triton.jit
+def add_kernel(
+    x_ptr,
+    y_ptr,
+    output_ptr,
+    n_element : int,
+    BLOCK_SIZE: tl.constexpr,) 
+    """
+    the decorator tells python to compile it on gpu,
+
+    the inputs are pointers, not full tensors to the memory to the first entry of 
+    x, y and output,
+
+    BLOCK_SIZE will be regarded as an already compiled constant at compile time, meaning
+    #TODO explain it better
+
+    """
+    #there is multiple instance of the kernel running in parallel,
+    #to see which chunk of the actual data is it mine to calculate
+
+    PID = tl.program_id(axis=0) # 0 referring to the grid (4,) so PID is going to be either 0 one two or 3
+
+
 
 def test_add_kernel(size,atol=1e-3,rtol=1e-3):
     """
